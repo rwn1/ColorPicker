ae9f8522ffbe3787bb7ef406318b4fdeaf2df6aa
rwn1 <martinkantar42@gmail.com>
2026-01-02 19:18:42 +0100

    Color.Picker.Blazor.App - New implementations for Blazor.

---
+++ ColorPicker.Blazor.App3/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ColorPicker.Core.Models;
+using System.Drawing;
+
+namespace ColorPicker.Blazor.App3.ViewModels
+{
+    internal class MainViewModel : ObservableObject
+    {
+        private Color _selectedColor;
+        /// <summary>
+        /// Gets or sets the selected color.
+        /// </summary>
+        public Color SelectedColor
+        {
+            get => _selectedColor;
+            set
+            {
+                if (_selectedColor.ToString() != value.ToString())
+                {
+                    _selectedColor = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            _selectedColor = Color.Teal;
+        }
+    }
+}
